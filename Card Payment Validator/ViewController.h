f3ad7bcf255dbf2af81379438c7df92419648d09
AbdulKareem Khaja Mohammed <abd.khaja@gmail.com>
2016-04-24 00:07:22 -0700

    Initial Commit

---
+++ Card Payment Validator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Card Payment Validator
+//
+//  Created by Taiyaba Sultana on 4/24/16.
+//  Copyright © 2016 Abdul Kareem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Card Payment Validator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Card Payment Validator
+//
+//  Created by Taiyaba Sultana on 4/24/16.
+//  Copyright © 2016 Abdul Kareem. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
